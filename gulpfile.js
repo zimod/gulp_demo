c609647f8fd9ddd404d2a26f965e01f362c014d2
Zimo Dong <andy199354@gmail.com>
2018-01-02 16:11:56 -0800

    added related dependecies in package.json and created gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      uglify = require('gulp-uglify'),
+      rename = require('gulp-rename'),
+      sass = require('gulp-sass'),
+      maps = require('gulp-sourcemaps'),
+      del = require('del'),
+      connect = require('gulp-connect');
